c80b19f281263890296d7291e5b98963ca0cd050
vagrant <logan.davies181@gmail.com>
2023-07-02 04:23:31 +0000

    complete first test case

--- Tests/XFCC.Test/XFCCTest.cs
+++ Tests/XFCC.Test/XFCCTest.cs
@@ -14,6 +14,14 @@ public class XFCCTest
         Assert.NotNull(parser);
 
         var value = parser.Parse();
-        Assert.Equal("http://frontend.lyft.com", value.Elements[0].By);
+        var elements = value.Elements;
+        Assert.Single(elements);
+        Assert.Equal("http://frontend.lyft.com", elements[0].By);
+        Assert.Equal("468ed33be74eee6556d90c0149c1309e9ba61d6425303443c0748a02dd8de688", elements[0].Hash);
+        Assert.Null(elements[0].Cert);
+        Assert.Null(elements[0].Chain);
+        Assert.Equal("/C=US/ST=CA/L=San Francisco/OU=Lyft/CN=Test Client", elements[0].Subject);
+        Assert.Equal("http://testclient.lyft.com", elements[0].URI);
+        Assert.Null(elements[0].DNS);
     }
 }
