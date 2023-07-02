b44fc23a777c6ecec45fc4861acd87287f1d0002
vagrant <logan.davies181@gmail.com>
2023-07-02 04:34:24 +0000

    add second test case

--- Tests/XFCC.Test/XFCCTest.cs
+++ Tests/XFCC.Test/XFCCTest.cs
@@ -5,6 +5,9 @@ using Xunit;
 
 public class XFCCTest
 {
+    // Test cases are examples from Envoy docs
+    // https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_conn_man/headers#x-forwarded-client-cert
+
     [Fact]
     public void Parse_Case1()
     {
@@ -23,5 +26,37 @@ public class XFCCTest
         Assert.Equal("/C=US/ST=CA/L=San Francisco/OU=Lyft/CN=Test Client", elements[0].Subject);
         Assert.Equal("http://testclient.lyft.com", elements[0].URI);
         Assert.Null(elements[0].DNS);
+    }
+
+    [Fact]
+    public void Parse_Case2()
+    {
+        var input = "By=http://frontend.lyft.com;Hash=468ed33be74eee6556d90c0149c1309e9ba61d6425303443c0748a02dd8de688;URI=http://testclient.lyft.com,By=http://backend.lyft.com;Hash=9ba61d6425303443c0748a02dd8de688468ed33be74eee6556d90c0149c1309e;URI=http://frontend.lyft.com";
+        var parser = new Parser(input);
+
+        Assert.NotNull(parser);
+
+        var value = parser.Parse();
+        var elements = value.Elements;
+
+        Assert.Equal(2, elements.Count);
+
+        // element 1
+        Assert.Equal("http://frontend.lyft.com", elements[0].By);
+        Assert.Equal("468ed33be74eee6556d90c0149c1309e9ba61d6425303443c0748a02dd8de688", elements[0].Hash);
+        Assert.Null(elements[0].Cert);
+        Assert.Null(elements[0].Chain);
+        Assert.Null(elements[0].Subject);
+        Assert.Equal("http://testclient.lyft.com", elements[0].URI);
+        Assert.Null(elements[0].DNS);
+
+        // element 2
+        Assert.Equal("http://backend.lyft.com", elements[1].By);
+        Assert.Equal("9ba61d6425303443c0748a02dd8de688468ed33be74eee6556d90c0149c1309e", elements[1].Hash);
+        Assert.Null(elements[1].Cert);
+        Assert.Null(elements[1].Chain);
+        Assert.Null(elements[1].Subject);
+        Assert.Equal("http://frontend.lyft.com", elements[1].URI);
+        Assert.Null(elements[1].DNS);
     }
 }
