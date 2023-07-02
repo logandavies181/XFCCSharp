fabbd273b91b12f0749dc4b8a642055d22b9aedf
vagrant <logan.davies181@gmail.com>
2023-07-02 02:04:02 +0000

    add builder and some parse methods

--- Source/XFCC/XFCC.cs
+++ Source/XFCC/XFCC.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text;
 
 namespace XFCC;
@@ -17,17 +18,10 @@ public class Parser
         public static readonly int backslash = (int)"\\"[0];
         public static readonly int doublequote = (int)"\""[0];
         public static readonly int eof = -1;
-    }
-
-    private static class Keys
-    {
-        public static readonly string By = "By";
-        public static readonly string Hash = "Hash";
-        public static readonly string Cert = "Cert";
-        public static readonly string Chain = "Chain";
-        public static readonly string Subject = "Subject";
-        public static readonly string URI = "URI";
-        public static readonly string DNS = "DNS";
+        public static readonly int a = (int)"a"[0];
+        public static readonly int z = (int)"z"[0];
+        public static readonly int A = (int)"A"[0];
+        public static readonly int Z = (int)"Z"[0];
     }
 
     ///
@@ -38,17 +32,49 @@ public class Parser
     }
 
     ///
-    public Value Parse()
+    public Value parse()
     {
         while (true)
         {
             int next = this.read();
-            switch (next)
+            if (next == Tokens.eof)
             {
-                case Tokens.eof:
-                    break;
+                break;
+            }
+            else if (isIdent(next))
+            {
+                unread();
+                var ident = this.readIdent();
             }
         }
+
+        return new Value();
+    }
+
+    private string readIdent()
+    {
+        var sb = new StringBuilder()
+
+        while (true)
+        {
+            int next = this.read();
+            if (next == Tokens.eof)
+            {
+                break;
+            }
+            else if (isIdent(next))
+            {
+                unread();
+                var ident = this.readIdent();
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    private bool isIdent(int c)
+    {
+        return (c >= Tokens.a && c <= Tokens.z) || (c >= Tokens.A && c <= Tokens.Z);
     }
 
     private int read()
@@ -78,27 +104,88 @@ public class Parser
 }
 
 ///
-public class Value
-{
+public class Value {
     ///
     public List<Element> elements = new List<Element>();
 }
 
 ///
-public class Element
+public class Element {
+    ///
+    public readonly string? By;
+    ///
+    public readonly string? Hash;
+    ///
+    public readonly string? Cert;
+    ///
+    public readonly string? Chain;
+    ///
+    public readonly string? Subject;
+    ///
+    public readonly string? URI;
+    ///
+    public readonly string? DNS;
+
+    ///
+    public Element(string? By, string? Hash, string? Cert, string? Chain, string? Subject, string? URI, string? DNS)
+    {
+        this.By = By;
+        this.Hash = Hash;
+        this.Cert = Cert;
+        this.Chain = Chain;
+        this.Subject = Subject;
+        this.URI = URI;
+    }
+}
+
+///
+public class ElementBuilder
 {
     ///
-    public readonly string? by;
+    private Dictionary<string, string> element = new Dictionary<string, string>();
+
     ///
-    public readonly string? hash;
+    public void add(string key, string value)
+    {
+        // TODO: raise exception if unknown key
+        element.Add(key, value);
+    }
+
     ///
-    public readonly string? cert;
+    public Element build()
+    {
+        return new Element(
+                element.GetValueOrDefault(Keys.By),
+                element.GetValueOrDefault(Keys.Hash),
+                element.GetValueOrDefault(Keys.Cert),
+                element.GetValueOrDefault(Keys.Chain),
+                element.GetValueOrDefault(Keys.Subject),
+                element.GetValueOrDefault(Keys.URI),
+                element.GetValueOrDefault(Keys.DNS)
+                );
+    }
+
     ///
-    public readonly string? chain;
+    public void reset()
+    {
+        element = new Dictionary<string, string>();
+    }
+}
+
+///
+public static class Keys {
     ///
-    public readonly string? subject;
+    public static readonly string By = "By";
     ///
-    public readonly string? uri;
+    public static readonly string Hash = "Hash";
     ///
-    public readonly string? dns;
+    public static readonly string Cert = "Cert";
+    ///
+    public static readonly string Chain = "Chain";
+    ///
+    public static readonly string Subject = "Subject";
+    ///
+    public static readonly string URI = "URI";
+    ///
+    public static readonly string DNS = "DNS";
 }
