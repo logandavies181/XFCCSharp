1066ea3e7b6ad0c91d06db47c7d464baa366089d
vagrant <logan.davies181@gmail.com>
2023-07-02 04:43:19 +0000

    add custom test cases

--- Tests/XFCC.Test/XFCCTest.cs
+++ Tests/XFCC.Test/XFCCTest.cs
@@ -59,4 +59,54 @@ public class XFCCTest
         Assert.Equal("http://frontend.lyft.com", elements[1].URI);
         Assert.Null(elements[1].DNS);
     }
+
+    [Fact]
+    public void Parse_CustomCase_QuotedDelimiters()
+    {
+        // by = `,;=\"`
+        var input = "By=\",;=\\\"\"";
+        var parser = new Parser(input);
+
+        Assert.NotNull(parser);
+
+        var value = parser.Parse();
+        var elements = value.Elements;
+
+        Assert.Single(elements);
+
+        // element 1
+        Assert.Equal(",;=\"", elements[0].By);
+    }
+
+    [Fact]
+    public void Parse_CustomCase_EmptyValues()
+    {
+        var input = ",";
+        var parser = new Parser(input);
+
+        Assert.NotNull(parser);
+
+        var value = parser.Parse();
+        var elements = value.Elements;
+
+        Assert.Equal(2, elements.Count);
+
+        // element 1
+        Assert.Null(elements[0].By);
+        Assert.Null(elements[0].Hash);
+        Assert.Null(elements[0].Cert);
+        Assert.Null(elements[0].Chain);
+        Assert.Null(elements[0].Subject);
+        Assert.Null(elements[0].URI);
+        Assert.Null(elements[0].DNS);
+        //
+        // element 1
+        Assert.Null(elements[1].By);
+        Assert.Null(elements[1].Hash);
+        Assert.Null(elements[1].Cert);
+        Assert.Null(elements[1].Chain);
+        Assert.Null(elements[1].Subject);
+        Assert.Null(elements[1].URI);
+        Assert.Null(elements[1].DNS);
+    }
 }
